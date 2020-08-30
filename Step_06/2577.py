429c1e826162fd087f297c4c14c6dd278856dcac
MingyuKim-2933 <68273065+MingyuKim-2933@users.noreply.github.com>
2020-08-30 21:48:16 +0900

    list에서 count() 사용

--- Step_06/2577.py
+++ Step_06/2577.py
@@ -2,16 +2,20 @@ A=int(input())
 B=int(input())
 C=int(input())
 multi = str(A * B * C)
-result = []
-for i in range(10):
-    result.append(0)
 
 for j in range(10):
-    for k in range(len(multi)):
-        if multi[k] == str(j):
-            result[j] += 1
+    print(multi.count(str(j)))
 
-for l in range(10):
-    print(result[l])
+# result = []
+# for i in range(10):
+#     result.append(0)
+#
+# for j in range(10):
+#     for k in range(len(multi)):
+#         if multi[k] == str(j):
+#             result[j] += 1
+#
+# for l in range(10):
+#     print(result[l])
 
 
